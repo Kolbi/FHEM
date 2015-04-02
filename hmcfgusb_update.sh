1694ef9f0f96044892a79c6a1b3fc91c47aa1310
Kolbi <MatthiasKolb@2-wheel-planet.de>
2015-04-02 12:47:09 +0200

    Update hmcfgusb_update.sh
    added some comments

--- hmcfgusb_update.sh
+++ hmcfgusb_update.sh
@@ -4,16 +4,21 @@
 # Setting up directories
 PFAD=/opt/hmcfgusb
 
+# Go to hmcfgusb directory
 cd $PFAD
 
+# Download latest commits
 sudo git pull git://git.zerfleddert.de/hmcfgusb
 
+# Compile hmcfgusb
 sudo make
 
+# Stop hmland
 sudo service hmland stop
 
 wait 5
 
+#Start hmland
 sudo service hmland start
 
 exit 0
