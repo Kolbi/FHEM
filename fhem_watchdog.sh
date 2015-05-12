564a06042f03a689604119371b34bae84562a8b2
Kolbi <MatthiasKolb@2-wheel-planet.de>
2015-05-12 14:06:44 +0200

    Update fhem_watchdog.sh
    added some nice features. Log downtime and send mail.

--- fhem_watchdog.sh
+++ fhem_watchdog.sh
@@ -4,6 +4,27 @@
 # sudo crontab -e
 # */5 * * * * /opt/fhem/fhem_watchdog.sh
 
+# Setting logfile
+FHEM_LOG=/var/log/fhem_watchdog.log
+
+# Log downtimes? (1=Yes, 0=No)
+LOG_DOWNTIME=0
+
+# Send mail? (1=Yes, 0=No)
+MAIL_SEND=0
+
+MAIL_SENDER="mail@mail.com"
+MAIL_RECEIVER="receiver@mail.com"
+MAIL_TITLE="Start FHEM"
+MAIL_TEXT="FHEM has been started..."
+MAIL_SMTP="smtp.mail.de"
+MAIL_ACCOUNT="mail@mail.com"
+MAIL_PASSWORD="securepassword"
+
+# Send log with mail? LOG_DOWNTIME must be 1. (1=Yes, 0=No)
+MAIL_ATTACH=0
+
+# Beginn script
 cnt=$(ps -ef | grep -v grep | grep fhem.pl | wc -l);
 
 if test $cnt -gt 0 ; 
@@ -12,10 +33,23 @@ if test $cnt -gt 0 ;
     exit 0
   else
     echo  "FHEM not running. starting FHEM...";
+    if [ $LOG_DOWNTIME = 1 ];
+    then
+      echo `date` "FHEM is not running. starting FHEM" >> $FHEM_LOG;
+    else
+      echo `date` "FHEM is not running. starting FHEM";
+    fi
     sudo service fhem start & sleep 5;
     # Install sendEmail
     # sudo apt-get install sendEmail
-    #sendEmail -f '[Absender]' -t '[Empfänger]' -u '[Titel]' -m '[Text] -a '[Anhang]' -s '[SMTP-Server]' -xu '[KONTO]' -xp '[PASSWORT]' -o tls=auto -o message-charset=utf-8;
-    #sendEmail -f '[Absender]' -t '[Empfänger]' -u 'Start FHEM' -m 'FHEM has been started...' -s '[SMTP-Server]' -xu '[KONTO]' -xp '[PASSWORT]' -o tls=auto -o message-charset=utf-8;
+    if [ $MAIL_SEND = 1 ];
+    then
+      if [ $MAIL_ATTACH = 1 ];
+      then
+        sendEmail -f '$MAIL_SENDER' -t '$MAIL_RECEIVER' -u '$MAIL_TITLE' -m '$MAIL_TEXT' -a '$FHEM_LOG' -s '$MAIL_SMTP' -xu '$MAIL_ACCOUNT' -xp '$MAIL_PASSWORD' -o tls=auto -o message-charset=utf-8;
+      else
+        sendEmail -f '$MAIL_SENDER' -t '$MAIL_RECEIVER' -u '$MAIL_TITLE' -m '$MAIL_TEXT' -s '$MAIL_SMTP' -xu '$MAIL_ACCOUNT' -xp '$MAIL_PASSWORD' -o tls=auto -o message-charset=utf-8;
+      fi
+    fi
     exit 0
 fi
