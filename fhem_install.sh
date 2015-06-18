fad4bd26aba516e269b1d13f79fbfe38f7e43f57
Kolbi <MatthiasKolb@2-wheel-planet.de>
2015-06-18 17:38:27 +0200

    Update fhem_install.sh

--- fhem_install.sh
+++ fhem_install.sh
@@ -10,6 +10,9 @@ sudo apt-get dist-upgrade -y
 # Install needed packages
 sudo apt-get install libusb-1.0-0-dev make gcc git-core build-essential perl libdevice-serialport-perl libio-socket-ssl-perl -y
 
+# Fehlende Abhänigkeiten installieren
+sudo apt-get install -f
+
 # Download FHEM
 # fhem-X.Y.deb bitte mit der aktuellsten, stabilen Version ersetzen
 # http://fhem.de/fhem.html#Download
@@ -19,9 +22,6 @@ wget  http://fhem.de/fhem-5.6.deb
 sudo dpkg -i fhem-5.6.deb
 # Löschen des heruntergeladenen DEB-Paketes (optional)
 rm fhem-5.6.deb
-
-# Fehlende Abhänigkeiten installieren
-sudo apt-get install -f
 
 exit 0
 
