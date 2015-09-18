ae87708d0fb9936bfe74ed3a64fa4be1392536d1
Kolbi <MatthiasKolb@2-wheel-planet.de>
2015-09-18 17:44:58 +0200

    Update homebridge.sh

--- homebridge.sh
+++ homebridge.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+# Homebridge Installation auf einem Raspberry Pi
 
-#http://pimatic.org/guide/getting-started/installation/
+# http://pimatic.org/guide/getting-started/installation/
 wget http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-arm-pi.tar.gz -P /tmp
 cd /usr/local
 sudo tar xzvf /tmp/node-v0.10.28-linux-arm-pi.tar.gz --strip=1
 cd /home/pi
 
-#https://github.com/nfarina/homebridge
+# Prüfung welche nodejs version ist installiert
+# /usr/bin/env node --version
+
+# https://github.com/nfarina/homebridge
 git clone https://github.com/nfarina/homebridge.git
 cd homebridge
 npm install
