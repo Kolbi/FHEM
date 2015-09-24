f53ec9cc4046458d799b22972f149fb3e3247295
Kolbi <MatthiasKolb@2-wheel-planet.de>
2015-09-24 08:25:38 +0200

    Update homebridge.sh

--- homebridge.sh
+++ homebridge.sh
@@ -29,3 +29,44 @@ npm install
 #npm update
 #oder 
 #npm install
+
+
+## Homebridge Startskript
+
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: 		 homebridge
+# Required-Start:    $network $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time for homebridge
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
+export PATH=$PATH:/usr/local/bin
+export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
+
+case "$1" in
+  start)
+  exec forever --sourceDir=/home/pi/homebridge -p /root/.forever start app.js
+  ;;
+
+  stop)
+  exec forever stop --sourceDir=/home/pi/homebridge app.js
+  ;;
+  
+  *)
+  echo "Usage: /etc/init.d/homebridge {start|stop}"
+  exit 1
+  ;;
+esac
+
+exit 0
+
+##
+
+##
+sudo chmod 755 /etc/init.d/hombebridge
+sudo chmod +x hombebridge
+sudo update-rc.d hombebridge defaults
