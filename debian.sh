b38aec17a7e51498365599d3d99c3e2c5d431f45
Kolbi <MatthiasKolb@2-wheel-planet.de>
2016-06-20 20:24:50 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -4,4 +4,5 @@ su
 apt-get update && apt-get install sudo
 #
 adduser [user] sudo
+#https://www.privateinternetaccess.com/forum/discussion/18063/debian-8-1-0-jessie-sudo-fix-not-installed-by-default
 reboot
